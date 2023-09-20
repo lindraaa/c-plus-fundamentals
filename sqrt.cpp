5c1792f5003017e9074c050cb3f160263d239c3b
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-20 17:31:03 +0800

    Create sqrt.cpp

---
+++ sqrt.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath> // this library is needed to be able to use the sqrt function
+using namespace std;
+int main()
+{
+	double ans = 0.0;
+	double num, ans2;
+	
+	//sqrt is a built in function
+	ans = sqrt(64); //pow is one example of a function
+	cout << ans <<endl <<endl;
+	
+	cout <<"The task used to input to input value in determining its square root";
+	cout <<"\nEnter the number: ",
+	cin >> num;
+	  
+	ans2= sqrt(num);
+	
+	cout <<"\nThe square root of " << num  <<" is " << ans2 <<endl;
+
+ 
+	return 0;
+}
