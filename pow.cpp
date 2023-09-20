13c254144846b6fd5ecbe7e5b1a8c375eab9102a
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-20 17:30:47 +0800

    Create pow.cpp

---
+++ pow.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath> // this library is needed to be able to use the pow function
+using namespace std;
+int main()
+{
+	double cube = 0.0;
+	double base, exp;
+	
+	//pow is a built in function
+	cube = pow(4.0, 3); //pow is one example of a function
+	cout << cube <<endl <<endl;
+	
+	cout <<"The task used to input the basse and the exponent : \n";
+	cout <<"Enter the base value: ",
+	cin >> base;  
+	
+	cout <<"\nEnter the exponent value: ";
+	cin >> exp;
+	
+	cout <<"The answer is: " << pow(base,exp) <<endl;
+	
+	
+	return 0;
+}
