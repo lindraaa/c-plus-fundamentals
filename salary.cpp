0a36286e5c99da40916c96fe99a23e04e7fedaf4
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-20 17:31:46 +0800

    Create salary.cpp

---
+++ salary.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+float overtime(float, float, float, float, float);
+int main()
+{	
+	float prhr,regularhr, overtimehr, weekslry, otsalary;
+	cout << "Enter your hourly pay rate: ";
+	cin >> prhr;
+	cout << "\nEnter regular hours worked: ";
+	cin >> regularhr;
+	cout << "\nEnter overtime hours worked: ";
+	cin >> overtimehr;
+	weekslry= prhr* regularhr;
+	cout << "\n\nWeekly Salary: ";
+	cout << weekslry;
+	
+	otsalary = (regularhr* prhr)+ (overtimehr* 1.5*prhr);
+	cout << "\nWeekly Salary with overtime pay : " << otsalary;
+	
+return 0;
+}
+
+float overtime (float, float, float, float, float)
+{
+	float prhr,regularhr, overtimehr, weeks, otsalary;
+	otsalary = (regularhr* prhr)+ (overtimehr* 1.5*prhr);
+	
+	return otsalary;
+	
+}
