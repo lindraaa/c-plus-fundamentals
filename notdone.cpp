315d03d803c422f6379eb8c244a640d843782269
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-20 17:32:05 +0800

    Create notdone.cpp

---
+++ notdone.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//You can create a user/programmer- defined funtion before the main() function
+//function declaration section:
+void add();
+
+//main function
+int main() //the name of the function is main
+{
+	int x = 5;
+	add(); //function call  or calling a function (using its name)
+	cout <<endl;
+	
+	cout <<"Running the add() function 5x using a while loop\n";
+	while(x > 0)
+	
+	
+	
+	return 0;
+}
