29deb877bd4e982a689d35258299c76788391a4c
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-20 17:31:26 +0800

    Create myinvest.cpp

---
+++ myinvest.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float number;
+	cout <<"Input a number (1,2,3,4): ";
+	cin>> number;
+	
+	
+	while(number <= 3 )
+     {
+	   cout <<"Goodjob!";
+	   cout <<"\nInput another number again (1,2,3,4): ";
+	   cin >> number;
+	   
+	   
+	
+	 }
+		 if ( number>=5)
+	 while(number > 4 )
+	  {
+	  
+		cout << "Error";
+
+		cout <<"\nInput the number again: ";
+		cin >> number; 
+	  }
+
+	
+	
+	
+	return 0;
+}
