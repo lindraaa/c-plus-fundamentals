8f4a60e72c40a8845121494d570fcbac0c8f1f79
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-20 17:32:19 +0800

    Create postest.cpp

---
+++ postest.cpp
@@ -0,0 +1,20 @@
+//postest.cpp
+//postest loop - the condition is evaluated or tested first before the loop body
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x = 10;
+	
+	//postest loop structure using DO-WHILE loop
+	do
+	{
+		cout << x <<" ";
+		x = x - 1;
+	
+	}	while(x < 0); //loop condtion/test end of do-while loop include semicolon(;) after the WHILE CONDTION
+	
+		cout << endl << endl;
+		cout << "bye!";
+	return 0;	
+}
