df85fb3a9346d7e37afe4b89fab0986c94c8430e
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-21 14:19:16 +0800

    Create exe.cpp

---
+++ exe.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float price,taxrate,pttaxrate, tprice;
+  cout<<" Enter the price of 4k tv:"<<endl;
+  cin >> price;
+  cout <<" Enter the tax rate:"<<endl;
+  cin >> taxrate;
+  
+  
+  
+	
+
+return 0;
+}
