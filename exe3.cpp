03a616887b7abd685239da34ef18f4119afd58bd
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-21 14:18:06 +0800

    Create exe3.cpp

---
+++ exe3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float first, second, third, sum, av, pr;
+	
+	cout <<"Enter the three different integers: ";
+	cin >> first >> second >> third;
+	
+	sum = first + second + third;
+	cout <<"Sum is " << sum;
+	
+	av = sum/ 3;
+	cout <<"\nAverage is " << av;
+	
+	pr = first * second * third;
+	cout <<"\nProduct is "<< pr;
+	
+ 
+ 
+   if( first < second && first < third)
+   cout <<"\nSmallest is " << first;
+   
+   if( second < first && second < third)
+   cout <<"\nSmallest is " <<second;
+   
+   if(third < first && third < second)
+   cout <<"\nSmallest is " << third;   
+   
+   
+   
+    if(	first > second && first > third)
+    cout <<"\nLargest is " << first;
+    
+	if( second > first && second > third)
+	cout <<"\nLargest is " << second;
+	
+	if( third > first && third >second)
+	cout <<"\nLargest is " << third;    
+	
+	return 0;	
+}
