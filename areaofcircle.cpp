0d71074f6e9de6a0664560813a7a09f85a399db5
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-22 16:10:08 +0800

    Create areaofcircle.cpp

---
+++ areaofcircle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+  float number, areaofcircle, circumferenceofcircle;
+  cout <<"Enter the number:";
+  cin >>  number;
+  
+  if (number ==1)
+   {
+   areaofcircle = 3.14 * (number * number);
+   cout <<"\nThe area of a circle is " <<areaofcircle;
+   } 
+  else
+   {
+   circumferenceofcircle =  2 * 3.14 * number;
+   cout <<"\nThe circumference of a circle is " << circumferenceofcircle;
+    }
+	
+	return 0;
+}
