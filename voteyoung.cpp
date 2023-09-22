a1aa6da50b8917e2c975104319838a5123e35a4a
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-22 16:10:34 +0800

    Create voteyoung.cpp

---
+++ voteyoung.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float age;
+	char restatus;
+    cout <<"Enter the age: ";
+    cin >> age;
+    
+    if(age > 18)
+      {
+	   cout <<"\nEnter registration status(Y/N): ";
+       cin >> restatus;
+       if (toupper(restatus) ==  'Y')
+	     cout <<"\nYou can vote";
+	   else
+	     cout <<"\nYou must register before you can vote";
+      } 
+	 else 
+	cout <<"\nYou are to young to vote";   
+    
+	
+	return 0;	
+}
