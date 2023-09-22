4d2b15cbf5865c9a847589590c221018feca2dbf
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-22 16:12:12 +0800

    Create collegeadmision.cpp

---
+++ collegeadmision.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float gaverage, atestscore;
+	
+	cout <<"Enter the general point average: ";
+	cin >> gaverage;
+	cout <<"Enter the admission test score: ";
+	cin >> atestscore;
+	
+	
+	if(gaverage >= 75)
+	{
+		if(atestscore>=60)	
+		cout<<"\nAccept";
+	
+		else
+		
+		cout<<"\nReject";
+    }
+	else
+	
+	{
+	
+		if(atestscore >= 80)
+		cout<<"\nAccept";
+		
+		else
+	
+		cout<<"\nReject";	
+    }
+	
+	 
+	return 0;	
+}
