4fe9580f406e73cdf9bbde7fbc0f5924db866041
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-22 16:11:48 +0800

    Create cin2.cpp

---
+++ cin2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num;	// declare a integer(int) variable named num
+				// variables are storage of data in the computer memory (RAM)
+	
+	cout <<" Enter an integer:";
+	cin >> num; // taking input from the system's input device (keyboard) using the
+				// extraction operator (>>)
+	
+	cout << "The number:" <<num;	
+return 0;	
+
+}
