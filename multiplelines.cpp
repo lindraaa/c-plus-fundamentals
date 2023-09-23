e272ca933573815900f0c516d9ba6d1048aa72e4
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-23 19:59:53 +0800

    Create multiplelines.cpp

---
+++ multiplelines.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << endl <<"Hello World" <<endl;
+	cout << endl <<"Hi" <<endl;
+	cout << endl;
+	
+	
+	cout <<"The quick \n\n";
+	cout <<"brown fox jumps";
+	cout << "\n\nover the lazy dog";
+	cout <<"\n";
+return 0;
+}
