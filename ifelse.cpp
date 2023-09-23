f91724576da87ddbb23316a765f08f054ded478f
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-23 19:58:55 +0800

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	float hours, rate, gross;
+	
+	cout <<"Please enter the no. of hours worked for the week:";
+	cin >> hours;
+	
+	cout << "\nEnter the hourly rate:";
+	cin >> rate;
+	
+	gross = hours * rate;
+	
+	
+	if(hours <= 40)           // true path//
+    gross = hours * rate;
+ 
+ 	else  	              
+	{
+	 gross = hours * rate + (hours - 40) * rate / 2;
+	 cout << "\nYour gross pay with overtime is :" <<gross;
+    }
+	 	
+	cout << "\nThe gross pay for the employee is :" << gross ;
+	
+	
+	
+	return 0;
+}
