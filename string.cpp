8c1e990b304524d6c01f054477d510603f02ba40
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-25 19:53:49 +0800

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	
+	char cha1;
+	string str1;
+	
+	cout <<"Enter your name: " ;
+	getline (cin, str1);
+	
+	cout << "n\ Hello!" << str1;
+	cout <<endl;
+
+return 0;
+}
