ce0569bd74d698f25872883826846c340bd33e68
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-10-28 19:41:20 +0800

    Update multiplelines.cpp

--- multiplelines.cpp
+++ multiplelines.cpp
@@ -6,8 +6,7 @@ int main()
 	cout << endl <<"Hello World" <<endl;
 	cout << endl <<"Hi" <<endl;
 	cout << endl;
-	
-	
+
 	cout <<"The quick \n\n";
 	cout <<"brown fox jumps";
 	cout << "\n\nover the lazy dog";
